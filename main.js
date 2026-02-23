c1f7d80ef7eaabd3132836c78347dccae6c78108
yair shubeli <game8527690@gmail.com>
2026-02-23 14:53:13 +0200

    commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false 
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
